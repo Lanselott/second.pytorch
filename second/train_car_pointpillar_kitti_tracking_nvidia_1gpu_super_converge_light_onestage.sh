608d7c3b00524be3b20afee5b2ae5cc1b2161047
Lanselott <chenran1995@link.cuhk.edu.hk>
2019-12-20 15:26:25 +0800

    update

---
+++ second/train_car_pointpillar_kitti_tracking_nvidia_1gpu_super_converge_light_onestage.sh
@@ -0,0 +1,1 @@
+python ./pytorch/train_multi_frame.py train --config_path=./configs/car.fhd.onestage_kitti_tracking_nvidia_1gpu_9313.config --model_dir=./results/car_onestage_kitti_tracking_super_converge_1gpu --resume=True
