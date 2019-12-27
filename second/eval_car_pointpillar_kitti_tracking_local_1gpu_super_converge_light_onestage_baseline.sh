6e6b5b5f6e5c78c335f9d7cd69fab3aa24fc381d
Lanselott <chenran1995@link.cuhk.edu.hk>
2019-12-27 12:55:26 +0800

    update

---
+++ second/eval_car_pointpillar_kitti_tracking_local_1gpu_super_converge_light_onestage_baseline.sh
@@ -0,0 +1,1 @@
+python ./pytorch/train.py evaluate --config_path=./configs/car.fhd.onestage_kitti_tracking_local_1gpu_baseline.config --model_dir=./results/car_kitti_tracking_1gpu_super_converge_baseline --measure_time=True --batch_size=1
